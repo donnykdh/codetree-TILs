0dcabd714474d37c98aa18f2e324525fffc0261f
donnykdh <122787870+donnykdh@users.noreply.github.com>
2023-12-09 18:26:28 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231209/최고의 13위치/best-place-of-13.cpp
+++ 231209/최고의 13위치/best-place-of-13.cpp
@@ -15,7 +15,7 @@ int main() {
 
     int max_cnt = 0;
     for(int i = 0; i < n; i++)
-        for(int j = 0; j < n - 1; j++)
+        for(int j = 0; j < n - 2; j++)
             max_cnt = max(max_cnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2]);
     
     cout << max_cnt;
