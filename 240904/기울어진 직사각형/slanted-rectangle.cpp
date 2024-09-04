588fa48bd1cd1ae3efe524e989d3332f131da8b0
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-09-04 16:16:11 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/기울어진 직사각형/slanted-rectangle.cpp
+++ 240904/기울어진 직사각형/slanted-rectangle.cpp
@@ -3,93 +3,86 @@ using namespace std;
 
 int n;
 int arr[20][20];
-int sum = 0;
-int k1, k2;
 int MAX = 0;
 
-bool range(int i, int j){
+bool range(int i, int j) {
     return i >= 0 && i < n && j >= 0 && j < n;
 }
 
-void Fourth(int i, int j,int k1, int k2){
-    if(range((i+k2), (j+k2))){
-    for(int a = 1; a <= k2; a++){
-       
+// 4번째 방향: 대각선 ↘
+int Fourth(int i, int j, int k1, int k2, int sum) {
+    if (range(i + k2, j + k2)) {
+        for (int a = 1; a <= k2; a++) {
             sum += arr[i + a][j + a];
+        }
+    } else {
+        return 0;
     }
-    
-    //return sum;
-    }
+    return sum;
 }
 
-void Third(int i, int j, int k1, int k2){
-    if(range((i+k1), (j-k1))){
-    for(int a = 1; a <= k1; a++){
-        
+// 3번째 방향: 대각선 ↙
+int Third(int i, int j, int k1, int k2, int sum) {
+    if (range(i + k1, j - k1)) {
+        for (int a = 1; a <= k1; a++) {
             sum += arr[i + a][j - a];
+        }
+    } else {
+        return 0;
     }
-    Fourth((i+k1), (j-k1), k1, k2);
-    }
+    return Fourth(i + k1, j - k1, k1, k2, sum);
 }
 
-void Second(int i, int j, int k1, int k2){
-    if(range((i-k2), (j-k2))){
-
-    for(int a = 1; a <= k2; a++){
+// 2번째 방향: 대각선 ↖
+int Second(int i, int j, int k1, int k2, int sum) {
+    if (range(i - k2, j - k2)) {
+        for (int a = 1; a <= k2; a++) {
             sum += arr[i - a][j - a];
+        }
+    } else {
+        return 0;
     }
-    Third((i-k2), (j-k2), k1, k2);
-    }
+    return Third(i - k2, j - k2, k1, k2, sum);
 }
 
-void First(int i, int j, int k1, int k2){
-    sum = 0;
-    if(range((i-k1), (j+k1))){
-        for(int a = 1; a <= k1; a++){
-            
+// 1번째 방향: 대각선 ↗
+int First(int i, int j, int k1, int k2) {
+    int sum = 0;
+    if (range(i - k1, j + k1)) {
+        for (int a = 1; a <= k1; a++) {
             sum += arr[i - a][j + a];
         }
-        Second((i-k1), (j+k1), k1, k2);
+    } else {
+        return 0;
     }
+    return Second(i - k1, j + k1, k1, k2, sum);
 }
-
-
-
-
 
 int main() {
     cin >> n;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    
+    // 배열 입력
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
         }
     }
 
     // 시작점
-    int q;
-    int w;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            //MAX = 0;
-            sum = 0;
-            for(k1 = 1; k1 < n; k1++){
-                for(k2 = 1; k2 < n; k2++){
-                    First(i, j, k1, k2);
-                    if(sum >= MAX){
-                MAX = sum;
-                //q=i;
-                //w=j;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k1 = 1; k1 < n; k1++) {
+                for (int k2 = 1; k2 < n; k2++) {
+                    int sum = First(i, j, k1, k2);
+                    if (sum > MAX) {
+                        MAX = sum;
+                    }
                 }
-                //cout << sum <<endl;
             }
-            
         }
     }
-    //cout << q << endl << w << endl;
-    //cout << k1 << endl << k2 <<endl;
-    cout << MAX <<endl;
-
     
+    cout << MAX << endl;
+
     return 0;
 }
