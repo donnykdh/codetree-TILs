63d72d9c67d3be6ce92c70415a6b1e0f3cf2c540
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-09-04 11:30:51 +0900

    Title: 트로미노, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/트로미노/tromino.cpp
+++ 240904/트로미노/tromino.cpp
@@ -1,56 +1,80 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int n, m;
 int arr[200][200];
-int MAX = 0;
+int MIN = 1000;
+int MAX = 1;
 
-// 계단식 블럭: 2x2에서 한 칸을 제외하는 방식으로 최솟값을 뺌
-int stairCase(int a, int b) {
-    if (a + 1 >= n || b + 1 >= m) return 0;
-    
-    // 2x2 블럭의 4개 숫자 중에서 최솟값을 뺌
-    int sum = arr[a][b] + arr[a][b+1] + arr[a+1][b] + arr[a+1][b+1];
-    int minVal = min({arr[a][b], arr[a][b+1], arr[a+1][b], arr[a+1][b+1]});
-    
-    return sum - minVal;
+
+int firstCase(int a, int b){
+    int sum = 0;
+    MIN = 1000;
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            if(a+i >= n || b+j >= m){
+                return 0;
+            }
+            if(arr[a+i][b+j] <= MIN){
+                MIN = arr[a+i][b+j];
+            }
+            sum += arr[a+i][b+j];
+        }
+    }
+    return sum - MIN;
 }
 
-// 1x3 직선형 블럭 (가로, 세로 모두)
-int lineCase(int a, int b) {
-    int max_sum = 0;
-
-    // 1. 가로로 놓은 경우
-    if (b + 2 < m) {
-        int sum = arr[a][b] + arr[a][b+1] + arr[a][b+2];
-        max_sum = max(max_sum, sum);
+int secondCase(int a, int b){
+    int sum = 0;
+    if(b + 1 >= m || b + 2 >= m){
+        return 0;
     }
-
-    // 2. 세로로 놓은 경우
-    if (a + 2 < n) {
-        int sum = arr[a][b] + arr[a+1][b] + arr[a+2][b];
-        max_sum = max(max_sum, sum);
+    else{
+        return arr[a][b] + arr[a][b + 1] + arr[a][b + 2];
     }
-
-    return max_sum;
 }
+
+int thirdCase(int a, int b){
+    int sum = 0;
+    if(a + 1 >= n || a + 2 >= n){
+        return 0;
+    }
+    else{
+        return arr[a][b] + arr[a + 1][b] + arr[a + 2][b];
+    }
+}
+
 
 int main() {
     cin >> n >> m;
-
-    // 배열 입력
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
             cin >> arr[i][j];
         }
     }
 
-    // 모든 위치에서 각 블럭을 놓아보고 최대값 계산
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            MAX = max(MAX, stairCase(i, j)); // 계단식 블럭
-            MAX = max(MAX, lineCase(i, j));  // 1x3 블럭 (가로, 세로 포함)
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(firstCase(i, j) >= MAX){
+                MAX = firstCase(i, j);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(secondCase(i, j) >= MAX){
+                MAX = secondCase(i, j);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(thirdCase(i, j) >= MAX){
+                MAX = thirdCase(i, j);
+            }
         }
     }
 
