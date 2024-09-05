62d32350ecdffd171f1d6c9ed159d51d90ed3b3f
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-09-05 15:02:11 +0900

    Title: K번 최댓값으로 이동하기, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
+++ 240905/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -1,75 +1,129 @@
 #include <iostream>
+#include <queue>
+
+#define MAX_N 100
+#define DIR_NUM 4
+#define NOT_EXISTS make_pair(-1, -1)
+
 using namespace std;
 
-int n, k, r, c;
-int grid[100][100];
+int n, k;
+int grid[MAX_N][MAX_N];
 
-// 상하좌우 이동을 위한 dx, dy 배열
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+// 현재 위치
+pair<int, int> curr_cell;
 
-// 범위 체크 함수
-bool inRange(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < n;
+queue<pair<int, int>> q;
+bool visited[MAX_N][MAX_N];
+
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-// k번 이동을 시도하는 함수
-void move() {
-    for (int step = 0; step < k; step++) {
-        int maxVal = -1;
-        int nextR = -1, nextC = -1;
+bool CanGo(int x, int y, int target_num) {
+    return InRange(x, y) && !visited[x][y] && grid[x][y] < target_num;
+}
 
-        // 현재 위치에서 상하좌우로 이동 가능한 칸 탐색
-        for (int d = 0; d < 4; d++) {
-            int nr = r + dx[d];
-            int nc = c + dy[d];
+// visited 배열을 초기화 해줍니다.
+void InitializeVisited() {
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            visited[i][j] = false;
+}
 
-            if (inRange(nr, nc) && grid[nr][nc] < grid[r][c]) {
-                if (grid[nr][nc] > maxVal) {
-                    // 더 큰 값을 찾은 경우
-                    maxVal = grid[nr][nc];
-                    nextR = nr;
-                    nextC = nc;
-                } else if (grid[nr][nc] == maxVal) {
-                    // 값이 동일한 경우 행, 열 우선순위 적용
-                    if (nr < nextR || (nr == nextR && nc < nextC)) {
-                        nextR = nr;
-                        nextC = nc;
-                    }
-                }
+void BFS() {
+    int dx[DIR_NUM] = {0, 1, 0, -1};
+    int dy[DIR_NUM] = {1, 0, -1, 0};
+
+    int curr_x = curr_cell.first;
+    int curr_y = curr_cell.second;
+    
+    visited[curr_x][curr_y] = true;
+    q.push(curr_cell);
+    
+    int target_num = grid[curr_x][curr_y];
+    
+    // BFS 탐색을 수행합니다.
+    while(!q.empty()) {
+        pair<int, int> curr_pos = q.front();
+        int curr_x = curr_pos.first;
+        int curr_y = curr_pos.second;
+        q.pop();
+
+        for(int i = 0; i < DIR_NUM; i++) {
+            int new_x = curr_x + dx[i];
+            int new_y = curr_y + dy[i];
+
+            if(CanGo(new_x, new_y, target_num)) {
+                q.push(make_pair(new_x, new_y));
+                visited[new_x][new_y] = true;
             }
         }
+    }
+}
 
-        // 이동할 곳이 없으면 종료
-        if (nextR == -1) {
-            break;
+// best 위치를 새로운 위치로 바꿔줘야 하는지를 판단합니다.
+bool NeedUpdate(pair<int, int> best_pos, pair<int, int> new_pos) {
+    if(best_pos == NOT_EXISTS)
+        return true;
+
+    int best_x = best_pos.first;
+    int best_y = best_pos.second;
+    
+    int new_x = new_pos.first;
+    int new_y = new_pos.second;
+
+    if (grid[new_x][new_y] > grid[best_x][best_y]) return true;
+    if (grid[new_x][new_y] == grid[best_x][best_y]) {
+        if (new_x < best_x) return true;
+        if (new_x == best_x && new_y < best_y) return true;
+    }
+    
+    return false;
+}
+
+// 가장 우선순위가 높은 위치를 찾아 이동합니다.
+bool Move() {
+    InitializeVisited();
+    
+    BFS();
+    
+    pair<int, int> best_pos = NOT_EXISTS;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(!visited[i][j] || make_pair(i, j) == curr_cell)
+                continue;
+            
+            pair<int, int> new_pos = make_pair(i, j);
+            if(NeedUpdate(best_pos, new_pos))
+                best_pos = new_pos;
         }
-
-        // 다음 위치로 이동
-        r = nextR;
-        c = nextC;
+    }
+    
+    if(best_pos == NOT_EXISTS)
+        return false;
+    else {
+        curr_cell = best_pos;
+        return true;
     }
 }
 
 int main() {
-    // 입력 받기
     cin >> n >> k;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) 
             cin >> grid[i][j];
-        }
-    }
-
-    // 시작 위치 입력 받기 (1-based -> 0-based로 변환)
+    
+    int r, c;
     cin >> r >> c;
-    r--; c--;
-
-    // k번 이동 수행
-    move();
-
-    // 최종 위치 출력 (1-based로 변환)
-    cout << r  << " " << c  << endl;
-
-    return 0;
+    curr_cell = make_pair(r - 1, c - 1);
+    
+    while(k--) {
+        bool is_moved = Move();
+        if(!is_moved)
+            break;
+    }
+    
+    cout << curr_cell.first + 1 << " " << curr_cell.second + 1;
 }
