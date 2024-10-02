756f1abef7914d723ed2c19d69d5cc349246c896
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-10-02 13:47:57 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int a, b, n;
+int nums[9];
+vector<int> ans;
+
+int main() {
+    cin >> a >> b;
+    cin >> n;
+    int cnt = 0;
+    int dec = 0;
+
+    while(1){
+        nums[cnt] = n % 10;
+        cnt++;
+        n /= 10;
+        if(n == 0){
+            break;
+        }      
+    }
+    for(int i = 0; i < cnt; i++){
+        dec += pow(a, i);
+    }
+
+    //cout << dec<< endl;
+    while(1){
+        ans.push_back(dec % b);
+        dec /= b;
+        if(dec == 0){
+            break;
+        }
+    }
+
+    for(int i = ans.size()-1; i >= 0; i--){
+        cout << ans[i];
+    }
+    return 0;
+}
