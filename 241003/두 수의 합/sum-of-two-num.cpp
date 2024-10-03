14ee28c1f9dd4fea24b6a54a6f91504f48407e44
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-10-03 22:01:07 +0900

    Title: 두 수의 합, Time: 82ms, Memory: 6MB, Status: Passed - CodeTree

--- 241003/두 수의 합/sum-of-two-num.cpp
+++ 241003/두 수의 합/sum-of-two-num.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <unordered_map>
+
 using namespace std;
 
+#define MAX_N 100000
+
+// 변수 선언
 int n, k;
-int arr[100000];
-unordered_map<int, int> hm;
+int arr[MAX_N];
+unordered_map<long long, int> count;
 
 int main() {
-	int cnt = 0;
+    // 입력:
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
 
-	cin >> n >> k;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		hm[arr[i]] = k - arr[i];
-	}
+    int ans = 0;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = i+1; j < n; j++) {
-			if (arr[i] == hm[arr[j]]) {
-				cnt++;
-			}
-		}
-	}
+    // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+    for(int i = 0; i < n; i++) {
+        long long diff = (long long)k - arr[i];
+        // 가능한 모든 쌍의 수를 세어줍니다.
+        ans += count[diff];
 
-	cout << cnt << endl;
+        // 현재 숫자의 개수를 하나 증가시켜줍니다.
+        count[arr[i]]++;
+    }
 
-	return 0;
+    cout << ans;
+    return 0;
 }
