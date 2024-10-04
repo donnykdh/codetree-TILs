5efb30da192e79e4f3ab4abb957fe090cc8c892b
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-10-05 00:39:57 +0900

    Title: 숫자 빠르게 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241005/숫자 빠르게 찾기/find-number-fast.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[100000];
+
+
+int Check(int target){
+    int left = 0;
+    int right = n - 1;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if( target == arr[mid]){
+            return mid;
+        }
+        if(target < arr[mid]){
+            right = mid - 1;
+        }
+        if(target > arr[mid]){
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+
+}
+
+
+
+int main() {
+    
+    cin >> n >> m;
+
+    
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < m; i++){
+        int num;
+        cin >> num;
+        cout << Check(num) + 1 << endl;
+
+    }
+    return 0;
+}
