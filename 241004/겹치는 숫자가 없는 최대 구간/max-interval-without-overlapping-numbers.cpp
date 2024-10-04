d1525e7f7b739f8c113b8c45dc49263f82fff5a2
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-10-04 23:58:43 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241004/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[1000];
+int count[1000] = {};
+
+int main() {
+    
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+
+    int ans = 0;
+    int j = 0;
+    for(int i = 1; i <= n; i++){
+
+        while(j + 1 <= n && count[arr[j+1]] == 0){
+            count[arr[j+1]]++;
+            j++;
+        }
+
+        if((j - i + 1) > ans){
+            ans = j - i + 1;
+        }
+
+        count[arr[i]]--;
+
+    }
+
+    cout << ans << endl;
+    return 0;
+}
