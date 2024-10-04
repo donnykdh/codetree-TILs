76b2636dda0fa11d0cf26d2aaf4f123534b210f0
donnykdh <122787870+donnykdh@users.noreply.github.com>
2024-10-04 15:21:25 +0900

    Title: 낮은 지점들, Time: 53ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241004/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+
+int n;
+unordered_map<int, int> hm;
+
+
+int main() {
+    int sum = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x, y;
+        cin >> x >> y;
+        if(hm.find(x) == hm.end()){
+            hm[x] = y;
+            sum += hm[x];
+        }
+        else{
+            if(hm[x] >= y){
+                sum -= hm[x];
+                hm[x] = y;
+                sum += hm[x];
+            }
+        }
+
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
